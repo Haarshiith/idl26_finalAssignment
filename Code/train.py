d47bb4dc97e60f66d53587578ed5822060b1b223
Harshith Babu <harshith227@gmail.com>
2026-06-11 22:02:33 +0200

    Fix: Resolve ModuleNotFoundError by using relative local imports

--- Code/train.py
+++ Code/train.py
@@ -8,9 +8,9 @@ import json
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from Code.data import get_loaders
-import Code.models as models
-from Code.fit import Trainer
+from data import get_loaders
+import models
+from fit import Trainer
 
 def main():   
     with open("config.json", "r") as f:
