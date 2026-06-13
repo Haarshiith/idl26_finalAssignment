712410fedb22bf172bd08c621f61b379c70eab0d
Harshith Babu <harshith227@gmail.com>
2026-06-13 23:38:19 +0200

    Fix: Restore data augmentation to prevent ResNet18 memorization on static datasets

--- Code/fit.py
+++ Code/fit.py
@@ -28,10 +28,10 @@ class Trainer:
             elif images.size(1) == 1 and expected_channels == 3:
                 images = images.repeat(1, 3, 1, 1)
             
-            # augmentations = T.Compose([
-            #     T.RandomAffine(degrees=0, translate=(0.1, 0.1)) # Gentle pixel shifting
-            # ])
-            # images = augmentations(images)
+            augmentations = T.Compose([
+                T.RandomAffine(degrees=15, translate=(0.1, 0.1))
+            ])
+            images = augmentations(images)
             
             self.optimizer.zero_grad()
             outputs = self.model(images)
